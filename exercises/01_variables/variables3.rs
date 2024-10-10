ad3a43dbb4855baba2089e5bbeebed69e6cf2e60
yaonur <ya_onur@hotmail.com>
2024-10-10 22:53:50 +0300

    ss

--- exercises/01_variables/variables3.rs
+++ exercises/01_variables/variables3.rs
@@ -1,6 +1,7 @@
 fn main() {
     // TODO: Change the line below to fix the compiler error.
-    let x: i32;
+    let x: i32 = 32;
+    
 
     println!("Number {x}");
 }
--- exercises/03_if/if1.rs
+++ exercises/03_if/if1.rs
@@ -4,6 +4,13 @@ fn bigger(a: i32, b: i32) -> i32 {
     // Do not use:
     // - another function call
     // - additional variables
+    if a > b {
+        return a
+    } else if b > a {
+        return b
+    } else {
+        return a
+    }
 }
 
 fn main() {
